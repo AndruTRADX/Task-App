4e45a551837c80f7622e08354f7b59c43fb025ae
andrutradx <andrudeluxe@gmail.com>
2024-03-18 16:06:40 -0500

    My improvements

--- Program.cs
+++ Program.cs
@@ -7,28 +7,33 @@ namespace ToDo
 {
   internal class Program
   {
-    public static List<string> TaskList { get; set; }
+    public static List<string> TaskList { get; set; } = new List<string>();
 
     static void Main(string[] args)
     {
-      TaskList = new List<string>();
       int menuSelected;
 
       do
       {
         menuSelected = ShowMainMenu();
 
-        if ((MenuOptions)menuSelected == MenuOptions.Add)
+        switch ((MenuOptions)menuSelected)
         {
-          ShowMenuAdd();
-        }
-        else if ((MenuOptions)menuSelected == MenuOptions.Remove)
-        {
-          ShowMenuRemove();
-        }
-        else if ((MenuOptions)menuSelected == MenuOptions.List)
-        {
-          ShowMenuTasksList();
+          case MenuOptions.Add:
+            ShowMenuAdd();
+            break;
+          case MenuOptions.Remove:
+            ShowMenuRemove();
+            break;
+          case MenuOptions.List:
+            ShowMenuTasksList();
+            break;
+          case MenuOptions.Exit:
+            Console.WriteLine("Saliendo...");
+            break;
+          default:
+            Console.WriteLine("Opción no válida, por favor selecciona una opción válida");
+            break;
         }
       } while ((MenuOptions)menuSelected != MenuOptions.Exit);
     }
@@ -43,38 +48,46 @@ namespace ToDo
       Console.WriteLine("3. Tareas pendientes");
       Console.WriteLine("4. Salir");
 
-      // Read line
-      string inputOptionSelected = Console.ReadLine();
-      return Convert.ToInt32(inputOptionSelected);
+      if (int.TryParse(Console.ReadLine(), out int inputOptionSelected))
+      {
+        return inputOptionSelected;
+      }
+      else
+      {
+        PrintConsoleLines(30);
+        Console.WriteLine("Error: Opción no válida, por favor ingrese un número.");
+        return -1;
+      }
     }
 
     public static void ShowMenuRemove()
     {
-      try
+
       {
         Console.WriteLine("Ingrese el número de la tarea a remover: ");
 
         PrintConsoleLines(30);
         PrintAllTasks(TaskList);
 
-        string inputOptionSelected = Console.ReadLine();
-
-        // Remove one position
-        int indexToRemove = Convert.ToInt32(inputOptionSelected) - 1;
-        if (indexToRemove > -1 || TaskList.Count > 0)
+        PrintConsoleLines(30);
+        if (int.TryParse(Console.ReadLine(), out int inputOptionSelected))
         {
-          string task = TaskList[indexToRemove];
-          TaskList.RemoveAt(indexToRemove);
-          Console.WriteLine("Tarea " + task + " eliminada");
+          int indexToRemove = inputOptionSelected - 1;
+          if (indexToRemove >= 0 && indexToRemove < TaskList.Count)
+          {
+            string task = TaskList[indexToRemove];
+            TaskList.RemoveAt(indexToRemove);
+            Console.WriteLine("Tarea " + task + " eliminada");
+          }
+          else
+          {
+            Console.WriteLine("Error: Por favor selecciona una tarea válida");
+          }
         }
-      }
-      catch (FormatException)
-      {
-        Console.WriteLine("Error: Ingrese un número válido.");
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine("Error: " + ex.Message);
+        else
+        {
+          Console.WriteLine("Error: Por favor ingrese un número válido.");
+        }
       }
     }
 
@@ -83,10 +96,8 @@ namespace ToDo
       try
       {
         Console.WriteLine("Ingrese el nombre de la tarea: ");
-
         string task = Console.ReadLine();
         TaskList.Add(task);
-
         Console.WriteLine("Tarea registrada");
       }
       catch (Exception ex)
@@ -99,7 +110,7 @@ namespace ToDo
     {
       PrintConsoleLines(30);
 
-      if (TaskList == null || TaskList.Count == 0)
+      if (TaskList.Count == 0)
       {
         Console.WriteLine("No hay tareas por realizar");
       }
